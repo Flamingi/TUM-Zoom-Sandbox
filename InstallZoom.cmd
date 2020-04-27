b75cdbea4e2ec4c1a017b214fca01f7445dbb36f
Flamingi <mingkwan1@freenet.de>
2020-04-27 13:45:46 +0200

    Uploaded sample files

---
+++ InstallZoom.cmd
@@ -0,0 +1,7 @@
+REM Download latest Zoom Installer
+curl -L "https://zoom.us/client/latest/ZoomInstaller.exe" --output C:\users\WDAGUtilityAccount\Desktop\ZoomInstaller.exe
+
+REM Install and run Zoom
+start C:\users\WDAGUtilityAccount\Desktop\ZoomInstaller.exe
+
+exit
